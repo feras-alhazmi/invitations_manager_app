71bd4f9599b3d9d59162a9eec1be6f945b413908
ahmed alhelali <ahmed43ali12@gmail.com>
2022-02-20 03:02:29 +0300

    UI improved

--- lib/screens/invitation_card_preview.dart
+++ lib/screens/invitation_card_preview.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/services.dart';
+
 import '../exports.dart';
 
 class InvitationCardPreview extends StatefulWidget {
@@ -20,7 +22,7 @@ class _InvitationCardPreviewState extends State<InvitationCardPreview> {
       "title": "Edit",
     },
     {
-      "icon": Icon(
+      "icon": const Icon(
         Icons.delete,
         color: Color(0xffE50000),
         size: 35,
@@ -36,182 +38,225 @@ class _InvitationCardPreviewState extends State<InvitationCardPreview> {
       "title": "Save",
     },
   ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        systemNavigationBarColor: Color(0xffEDF0F3),
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        leading: IconButton(
-          iconSize: 30,
-          icon: Icon(
-            Icons.arrow_back_ios,
-            color: Theme.of(context).backgroundColor,
-          ),
-          onPressed: () => Navigator.pop(context),
-        ),
-        title: Text(
-          "Preview Invitation Card",
-          style: CustomTextStyle()
-              .textStyle(18, Colors.white, fontWeight: FontWeight.w500),
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Stack(
         children: [
-          const Gap(68),
-          Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.45,
-                  width: double.infinity,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xffEDF0F3),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-                    child: Stack(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8),
-                          child: Align(
-                            alignment: Alignment.topLeft,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    const Icon(
-                                      MdiIcons.mapMarkerCheck,
-                                      color: Color(0xffA90641),
-                                      size: 30,
-                                    ),
-                                    Text(
-                                      widget.invitation["place"],
-                                      style: CustomTextStyle().textStyle(
-                                          14, const Color(0xff7A7A7A),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              borderRadius: const BorderRadius.vertical(bottom: Radius.circular(10)),
+            ),
+            height: MediaQuery.of(context).size.height*0.80,
+            
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              leading: IconButton(
+                iconSize: 30,
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: Theme.of(context).backgroundColor,
+                ),
+                onPressed: () => Navigator.pop(context),
+              ),
+              title: Text(
+                "Preview Invitation Card",
+                style: CustomTextStyle()
+                    .textStyle(18, Colors.white, fontWeight: FontWeight.w500),
+              ),
+              centerTitle: true,
+            ),
+            body: Column(
+              children: [
+                const Gap(68),
+                Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.45,
+                        width: double.infinity,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color(0xffEDF0F3),
+                            borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.black.withOpacity(0.15),
+                                    offset: const Offset(0,0),
+                                    spreadRadius: 2,
+                                    blurRadius: 2
+                                )
+                              ]
+                          ),
+                          padding:
+                          const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                          child: Stack(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 8),
+                                child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          const Icon(
+                                            MdiIcons.mapMarkerCheck,
+                                            color: Color(0xffA90641),
+                                            size: 30,
                                           ),
-                                    )
-                                  ],
-                                ),
-                                Container(
-                                  margin:
-                                  const EdgeInsets.symmetric(horizontal: 12),
-                                  height: 35,
-                                  width: 4,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color(0xffD1D1D1),
+                                          Text(
+                                            widget.invitation["place"],
+                                            style: CustomTextStyle().textStyle(
+                                              14, const Color(0xff7A7A7A),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      Container(
+                                        margin:
+                                        const EdgeInsets.symmetric(horizontal: 12),
+                                        height: 35,
+                                        width: 4,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(5),
+                                          color: const Color(0xffD1D1D1),
+                                        ),
+                                      ),
+                                      Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          const Icon(
+                                            MdiIcons.calendarCheck,
+                                            color: Color(0xffB9D800),
+                                            size: 30,
+                                          ),
+                                          Text(
+                                            Jiffy(widget.invitation["date"]).yMMMd,
+                                            style: CustomTextStyle().textStyle(
+                                              14, const Color(0xff7A7A7A),),
+                                          )
+                                        ],
+                                      )
+                                    ],
                                   ),
                                 ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Column(
                                   mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    const Icon(
-                                      MdiIcons.calendarCheck,
-                                      color: Color(0xffB9D800),
-                                      size: 30,
-                                    ),
+                                    const Gap(50),
                                     Text(
-                                      Jiffy(widget.invitation["date"]).yMMMd,
+                                      widget.invitation["name"],
                                       style: CustomTextStyle().textStyle(
-                                          14, const Color(0xff7A7A7A),),
-                                    )
+                                          18, Colors.black,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    const Gap(24),
+                                    Text(
+                                      widget.invitation["content"],
+                                      style: CustomTextStyle()
+                                          .textStyle(18, Colors.black),
+                                    ),
+                                    const Gap(24),
+                                    Text(
+                                      widget.invitation["regards"],
+                                      style: CustomTextStyle()
+                                          .textStyle(18, Colors.black),
+                                    ),
                                   ],
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Gap(50),
-                              Text(
-                                widget.invitation["name"],
-                                style: CustomTextStyle().textStyle(
-                                    18, Colors.black,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              const Gap(24),
-                              Text(
-                                widget.invitation["content"],
-                                style: CustomTextStyle()
-                                    .textStyle(18, Colors.black),
-                              ),
-                              const Gap(24),
-                              Text(
-                                widget.invitation["regards"],
-                                style: CustomTextStyle()
-                                    .textStyle(18, Colors.black),
-                              ),
+                                ),
+                              )
                             ],
                           ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Transform.translate(
-                  offset: const Offset(100, -70),
-                  child: Image.asset(
-                    "assets/images/dash_manager-x.png",
-                    width: 300,
-                    height: 260,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              )
-            ],
-          ),
-          const Gap(12),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(buttons.length, (index) {
-              return Container(
-                height: 130,
-                width: 106,
-                decoration: BoxDecoration(
-                  color: const Color(0xffEDF0F3),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-                margin: const EdgeInsets.symmetric(horizontal: 8),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      buttons[index]["icon"],
-                      const Gap(12),
-                      Text(
-                        buttons[index]["title"],
-                        style: CustomTextStyle().textStyle(18, Colors.black),
+                        ),
                       ),
-                    ]),
-              );
-            }),
-          )
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Transform.translate(
+                        offset: const Offset(100, -70),
+                        child: Image.asset(
+                          "assets/images/dash_manager-x.png",
+                          width: 300,
+                          height: 260,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const Gap(12),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(buttons.length, (index) {
+                    return Container(
+                      height: 130,
+                      width: 116,
+                      decoration: BoxDecoration(
+                        color: const Color(0xffEDF0F3),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.15),
+                            offset: const Offset(0,0),
+                            spreadRadius: 2,
+                            blurRadius: 2
+                          )
+                        ]
+                      ),
+                      padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+                      margin: const EdgeInsets.symmetric(horizontal: 4),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            buttons[index]["icon"],
+                            const Gap(12),
+                            Text(
+                              buttons[index]["title"],
+                              style: CustomTextStyle().textStyle(18, Colors.black),
+                            ),
+                          ]),
+                    );
+                  }),
+                )
+              ],
+            ),
+          ),
         ],
-      ),
+      )
     );
   }
 }
